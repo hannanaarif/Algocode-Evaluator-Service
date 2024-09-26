11791df4a4182d796eb89f6ccb94361de5783a7c
Aarif Hannan <hannanaarif@gmail.com>
2024-09-27 05:02:51 +0530

    updated the priority task

--- src/index.ts
+++ src/index.ts
@@ -21,19 +21,10 @@ app.listen(PORT,()=>{
         name: 'Sanket',
         company: 'Microsoft',
         position: 'SDE 2'
-    });
-    /*sampleQueueProducer('Samplejob', {
-        name: 'Sanket',
+    },2);
+    void sampleQueueProducer('Samplejob', {
+        name: 'Sarthak',
         company: 'Microsoft',
         position: 'SDE 2'
-    })
-    .then(() => {
-        console.log('Job added successfully.');
-    })
-    .catch((error) => {
-        console.error('Error adding job:', error);
-    });
-    
-    console.log('done sample worker');*/
-
+    },1);
 });
